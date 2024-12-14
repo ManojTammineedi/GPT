41ceb42d6b50702ee9ee5fa1a121b15186a1cc4e
ManojTammineedi <106574334+ManojTammineedi@users.noreply.github.com>
2024-12-14 12:06:32 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,12 @@ import 'package:gpt/auth/auth_gate.dart';
 import 'package:gpt/consts.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-
-const apiKey = 'AIzaSyALJCi62Ts6X1z_Syu1j5LadXqbT4SjPf0'; // Replace with your actual Gemini API key.
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Supabase.initialize(
-    url: 'https://idbgbaybqckkzdckvbza.supabase.co',
-    anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlkYmdiYXlicWNra3pkY2t2YnphIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM5MjYxMjQsImV4cCI6MjA0OTUwMjEyNH0.cLvDTUOmm44fW-1q4OPAwO7VPlSslpgNAdiRAsGAG9Q',
+ await Supabase.initialize(
+    url: 'YOUR_SUPABASE_URL',
+    anonKey: 'YOUR_SUPABASE_ANON_KEY',
   );
   Gemini.init(
     apiKey: GEMINI_API_KEY,
